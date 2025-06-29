1eb1217115ea7aa60b40a64217b9dd5bd65da2cd
dpvasani <vadanidarshan@gmail.com>
2025-06-29 23:59:24 +0530

    Docker Compose

--- Ts/src/index.ts
+++ Ts/src/index.ts
@@ -1,15 +1,43 @@
-// src/index.ts
 
 import http from "http";
+
+import { Redis } from "ioredis";
+import pg from "pg";
+
 import app from "./app/server";
 
-const PORT = process.env.PORT ? +process.env.PORT : 8000;
+async function init() {
+  try {
+    // Redis Connection
+    console.log(`Connecting Redis...`);
+    const redis = new Redis("redis://redis:6379", { lazyConnect: true });
+    await redis.connect();
+    console.log(`Redis Connection Success...`);
 
-const server = http.createServer(app);
+    // Postgresql Connection
+    console.log(`Connecting Postgres...`);
 
-server.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
-server.on("error", (error) => {
-  console.error("❌ Server error:", error);
-});
+    const { Client } = pg;
+    const client = new Client({
+      host: "db",
+      port: 5432,
+      database: "postgres",
+      user: "postgres",
+      password: "postgres",
+    });
+    await client.connect();
+
+    console.log(`Postgres Connection Success...`);
+
+    // Http Server Stuff
+    const PORT = process.env.PORT ? +process.env.PORT : 8000;
+    const server = http.createServer(app);
+    server.listen(PORT, () =>
+      console.log(`Http server is listening on PORT ${PORT}`)
+    );
+  } catch (err) {
+    console.log(`Error Starting Server`, err);
+  }
+}
+
+init();
