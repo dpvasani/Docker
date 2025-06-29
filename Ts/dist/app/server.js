5c0bf3ef567da8e87ec5812d2e9d75d523b20c8a
dpvasani <vadanidarshan@gmail.com>
2025-06-29 16:45:44 +0530

    Docker Custome Image With User Management

--- Ts/dist/app/server.js
+++ Ts/dist/app/server.js
@@ -1,15 +1,12 @@
 "use strict";
+// src/app/server.ts
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// DO NOT import 'app' from express
 const express_1 = __importDefault(require("express"));
-// Create app instance from express()
 const app = (0, express_1.default)();
-// Correct handler function
 app.get("/", (_req, res) => {
-    res.json({ message: "Hello from TypeScript Server" });
+    res.json({ message: "Hello from TypeScript Express Server" });
 });
-// Export the app
 exports.default = app;
--- Ts/src/app/server.ts
+++ Ts/src/app/server.ts
@@ -1,13 +1,11 @@
-// DO NOT import 'app' from express
+// src/app/server.ts
+
 import express from "express";
 
-// Create app instance from express()
 const app = express();
 
-// Correct handler function
 app.get("/", (_req, res) => {
-  res.json({ message: "Hello from TypeScript Server" });
+  res.json({ message: "Hello from TypeScript Express Server" });
 });
 
-// Export the app
 export default app;
